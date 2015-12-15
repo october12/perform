7e57e4e62d955c1b5b14b24b87a30e0db6d157ab
root <root@node38.(none)>
2015-12-15 23:04:55 +0800

    modified:   perf.php

--- perf.php
+++ perf.php
@@ -12,9 +12,11 @@
 //echo passthru("ls");
 //echo "<br>";
 if($_SERVER['REQUEST_METHOD']=="GET"){
-$str = shell_exec("sudo sh WEB-INF/unc_read.sh");
+$str = shell_exec("sudo sh WEB-INF/unc_read.sh 2>&1");
 $latency = explode("\n",$str);
-echo "{\"latency\":[".current($latency).",".next($latency)."]}";
+$str = shell_exec("sh WEB-INF/mem.sh");
+$mem = explode(" ",$str);
+echo "{\"latency\":[".current($latency).",".next($latency)."], \"memory\":[".current($mem).",".next($mem)."]}";
 }else if($_SERVER['REQUEST_METHOD']=="POST"){
 exec("sudo sh WEB-INF/redo_unc.sh");
 }
